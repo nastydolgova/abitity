c258199b111af68a4f3c702e119a7497441d9fb0
nastydolgova <nasty.dolgova@gmail.com>
2021-04-30 12:32:40 +0300

    lounge

--- js/popup-contact.js
+++ js/popup-contact.js
@@ -35,35 +35,63 @@ $(document).ready(function() {
     });
     $('.platform__title .popup__closeX').click(function() {
         $('.popup__meetings-platform').addClass("hidden");
+        $('.popup-background--lounge').addClass("hidden");
+
     });
     $('.popup__chat-invite').click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 500);
         $('.popup__meetings-platform').toggleClass("hidden");
+        $('.popup-background--lounge').toggleClass("hidden");
     });
     $('.popup-contact .popup__closeX').click(function(e) {
         e.preventDefault();
         $('.popup-contact').addClass("hidden");
         $('.popup__meetings-platform').addClass("hidden");
+        $('.popup-background--lounge').addClass("hidden");
         $('.select-card').addClass("hidden");
     });
     $('.for-step-1').click(function() {
+        $("html, body").animate({ scrollTop: 1250 }, 1000);
         $('.popup-contact').removeClass(" step-1 step-2 step-3 step-4");
         $('.popup-contact').addClass("step-1");
         $('.popup-contact').removeClass("hidden");
     });
     $('.for-step-2').click(function() {
+        $("html, body").animate({ scrollTop: 1250 }, 1000);
         $('.popup-contact').removeClass(" step-1 step-2 step-3 step-4");
         $('.popup-contact').addClass("step-2");
         $('.popup-contact').removeClass("hidden");
     });
     $('.for-step-3').click(function() {
+        $("html, body").animate({ scrollTop: 1250 }, 1000);
         $('.popup-contact').removeClass("step-1 step-2 step-3 step-4");
         $('.popup-contact').addClass("step-3");
         $('.popup-contact').removeClass("hidden");
     });
     $('.for-step-4').click(function() {
+        $("html, body").animate({ scrollTop: 1250 }, 1000);
         $('.popup-contact').removeClass("step-1 step-2 step-3 step-4");
         $('.popup-contact').addClass("step-4");
         $('.popup-contact').removeClass("hidden");
     });
-
+    $('.lounge-contact-1').click(function() {
+        $('.popup-contact').removeClass(" step-1 step-2 step-3 step-4");
+        $('.popup-contact').addClass("step-1");
+        $('.popup-contact').removeClass("hidden");
+    });
+    $('.lounge-contact-2').click(function() {
+        $('.popup-contact').removeClass(" step-1 step-2 step-3 step-4");
+        $('.popup-contact').addClass("step-2");
+        $('.popup-contact').removeClass("hidden");
+    });
+    $('.lounge-contact-3').click(function() {
+        $('.popup-contact').removeClass("step-1 step-2 step-3 step-4");
+        $('.popup-contact').addClass("step-3");
+        $('.popup-contact').removeClass("hidden");
+    });
+    $('.lounge-contact-4').click(function() {
+        $('.popup-contact').removeClass("step-1 step-2 step-3 step-4");
+        $('.popup-contact').addClass("step-4");
+        $('.popup-contact').removeClass("hidden");
+    });
 })
